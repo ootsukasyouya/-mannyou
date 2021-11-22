d336d87ca5899595d87bfc02628cf3b6202e3f2f
ootsukasyouya <rikkuhose@gmail.com>
2021-11-22 10:05:45 +0900

    修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -15,8 +15,7 @@ describe 'タスクモデル機能', type: :model do
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(title: '通過テスト', content: '通過テスト',status:'通過テスト',
-                        deadline: '2100-12-31',priority:0)
+        task = Task.new(title: '通過テスト', content: '通過テスト')
         expect(task).to be_valid
       end
     end
