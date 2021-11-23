e87321f8530ac1b917040f765ddb0af3b920ee7d
ootsukasyouya <rikkuhose@gmail.com>
2021-11-24 01:40:05 +0900

    rspec修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,4 +3,12 @@ FactoryBot.define do
     title { 'test_title' }
     content { 'test_content' }
   end
+  factory :second_task, class:Task do
+    title { 'test_title2' }
+    content { 'test_content2' }
+  end
+  factory :third_task, class:Task do
+    title { 'test_title3' }
+    content { 'test_content3' }
+  end
 end
