afb2dc11664f193e66a0126e0674e119a921d9bd
ootsukasyouya <rikkuhose@gmail.com>
2021-11-30 16:14:04 +0900

    テスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,15 +3,18 @@ FactoryBot.define do
     title { 'test_title' }
     content { 'test_content' }
     deadline {'2020-10-01'}
+    status {'未着手'}
   end
   factory :second_task, class:Task do
     title { 'test_title2' }
     content { 'test_content2' }
     deadline {'2020-11-01'}
+    status {'着手中'}
   end
   factory :third_task, class:Task do
     title { 'test_title3' }
     content { 'test_content3' }
     deadline {'2020-12-01'}
+    status {'完了'}
   end
 end
